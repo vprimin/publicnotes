741b7e33cac928c6e74edb93cd078005bbdec769
Slava Primin <primin@icloud.com>
2024-05-27 18:17:56 +0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Slava Primin",
+    pageTitle: "📔Slava Primin",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
