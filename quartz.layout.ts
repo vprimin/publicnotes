e47a14f2073662a996948bc47693201003ab13e9
Slava Primin <primin@icloud.com>
2024-05-27 17:35:26 +0300

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,8 +7,8 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      LinkedIn: "https://www.linkedin.com/in/vprimin/",
+      "My CV": "https://discord.gg/cRFFHYye7t",
     },
   }),
 }
