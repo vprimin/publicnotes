12415d517c1c43cc73b0ad940fe0c1613bdbe52a
Slava Primin <primin@icloud.com>
2024-05-28 16:29:11 +0300

    nu?

--- content/test note.md
+++ content/test note.md
@@ -1,7 +1,8 @@
 ---
-title: test note
+title: Как добавить картинку?
 draft: false
 tags:
   - zabbix
 ---
 Just want to understand how it works first
+![[SCR-20240528-kbyr.png]]![[logo2.png]]
--- quartz.config.ts
+++ quartz.config.ts
@@ -28,7 +28,7 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#fffde9",
+          light: "#fffdef",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
