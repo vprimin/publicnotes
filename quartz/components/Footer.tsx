c54435cc5a86b4afb4e112859aeac9dc6fe9530c
Slava Primin <primin@icloud.com>
2024-05-29 10:14:46 +0300

    Update Footer.tsx

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,27 +1,29 @@
-import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
+
+import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/footer.scss"
 import { version } from "../../package.json"
-import { i18n } from "../i18n"
 
 interface Options {
   links: Record<string, string>
 }
 
 export default ((opts?: Options) => {
-  const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
-  const links = opts?.links ?? []
-   const year = new Date().getFullYear()
+  function Footer({ displayClass }: QuartzComponentProps) {
+    const year = new Date().getFullYear()
+    const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
-        <hr />
         <p>
-         
-           © {year}
+          <small>
+            By <a href="https://www.pmcf.xyz">Pedro MC Fernandes</a>, unless otherwise stated or link-referenced.
+          </small>
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
             <li>
-              <a href={link}>{text}</a>
+              <small>
+                <a href={link}>{text}</a>
+              </small>
             </li>
           ))}
         </ul>
