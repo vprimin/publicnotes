86431cf2406ba2a9c50810422043d0b1958830f5
Slava Primin <99147956+vprimin@users.noreply.github.com>
2024-06-01 02:18:24 +0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -4,7 +4,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "Public Notes",
-    enableSPA: false,
+    enableSPA: true,
     enablePopovers: true,
     analytics: null,
     locale: "ru-RU",
